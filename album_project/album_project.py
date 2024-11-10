d03d963d80ec3b821cee5fb98b8b4862e16acee0
bryan <bryanvivanco97@outlook.com>
2024-11-10 01:24:32 -0500

    inicio proyecto con reflex

---
+++ album_project/album_project.py
@@ -0,0 +1,18 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+from album_project.pages.index import index
+
+
+
+# class State(rx.State):
+#     """The app state."""
+
+#     ...
+
+
+
+
+
+app = rx.App()
+app.add_page(index)
